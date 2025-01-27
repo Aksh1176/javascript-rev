357adec96125a00560090edafce32082b2056251
Akshat Choudhary <70104707+Aksh1176@users.noreply.github.com>
2025-01-27 15:43:07 +0000

    variables discussion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID = 12345689;
+let accountEmail = "akshat.google.com";
+var accountPassword = "1234"; 
+accountCity = "Patna";
+
+// accountID = 2   Not allowed
+console.log(accountID);
+accountEmail = "Shubham@google.com";
+accountPassword = "82748";
+accountCity = "Bengaluru";
+
+console.table([accountID,accountEmail,accountPassword,accountCity]);
+
+
+/* var should not be used as there is no concept of block scope & functional scope 
+
+*/
