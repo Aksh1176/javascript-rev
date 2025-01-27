dc0c2c14921ab488241738842849660802737263
Akshat Choudhary <70104707+Aksh1176@users.noreply.github.com>
2025-01-27 15:46:37 +0000

    variable discussion completed

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountID = 12345689;
 let accountEmail = "akshat.google.com";
 var accountPassword = "1234"; 
 accountCity = "Patna";
+let accountState;
 
 // accountID = 2   Not allowed
 console.log(accountID);
@@ -9,7 +10,8 @@ accountEmail = "Shubham@google.com";
 accountPassword = "82748";
 accountCity = "Bengaluru";
 
-console.table([accountID,accountEmail,accountPassword,accountCity]);
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
 
 
 /* var should not be used as there is no concept of block scope & functional scope 
