546d1131e1eabc513de1c533a7422e196ca2fd7c
Akshat Choudhary <70104707+Aksh1176@users.noreply.github.com>
2025-01-28 14:53:49 +0000

    datat types discussion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" //all codes will be traeated as newer version of javascript that is ES6.
+
+// alert(3+3) We are using node and not browser so this won't be working here.
+
+let name = "Akshat";
+let age = 28;
+let isLoggedIn = true;
+
+// Primitive data types -
+// number => 2 ^ 53;
+// Bigint
+// string
+// boolean
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// Non primitive -
+// object
+console.log("Akshat");
+
+console.table([typeof null,typeof undefined]) //object, undefined
