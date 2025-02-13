12c1fbb1999f0660943d15aa47e6c6a10c4718b9
Akshat Choudhary <70104707+Aksh1176@users.noreply.github.com>
2025-02-13 05:08:15 +0000

    data types summary

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+// The reason that an equality check == and comarisions (>,<,<=,>=) works differently.
+// Comparisions converts the number, treating it as a 0 wheras equality doesn't.
+
+
+
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+// Primitive- Call by value
+
+// String
+// number
+// null
+// undefined
+// Boolean
+// Symbol
+// BigInt
+
+
+
+// Non Primitive, Refrence Types- Call by refrence
+
+// Arrays
+// Objects
+// Functions
+
+
+/* Javascript is a dynamically typed language */
+
