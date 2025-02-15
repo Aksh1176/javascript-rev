8fb1372ab8bf3867474e472b776aebc08404ddec
Akshat Choudhary <70104707+Aksh1176@users.noreply.github.com>
2025-02-15 13:28:48 +0000

    strings discussion

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "Akshat";
+const repoCount = "50";
+console.log(`Hi my name is ${name} and repocount is ${repoCount}`);
+
+const newname = new String("Akshat");
+
+// Practice most of the commonly used string functions;
+
